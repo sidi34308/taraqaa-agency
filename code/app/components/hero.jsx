fd64c933b7fb2b2d2ee07c6b867f55231ea0d78d
Sidi Chaikh <sc2104008@qu.edu.qa>
2024-08-31 16:55:03 +0300

    update

--- code/app/components/hero.jsx
+++ code/app/components/hero.jsx
@@ -7,11 +7,13 @@ const Hero = () => {
   return (
     <section className="hero p-6 px-10">
       <div className="h-screen flex flex-col justify-center items-center text-center text-white">
-        <h1 className="sm:text-5xl text-3xl sm:text-center text-right font-bold mb-4 leading-tight sm:text-5xl sm:leading-normal sm:w-2/4">
-          أعد تعريف موقعك الإلكتروني وبرمجياتك مع !{" "}
+        <h1 className="sm:text-5xl text-3xl sm:text-center text-right font-bold mb-4 leading-tight  sm:w-2/4">
+          <span> أعد </span> <span>تعريف </span> <span>موقعك</span>{" "}
+          <span>الإلكتروني</span> <span>وبرمجياتك</span> <span>مع</span>{" "}
           <span className="text-[#CEEC33] ">تــرقى</span>
         </h1>
-        <p className="sm:text-lg sm:text-center text-sm text-right mb-8 max-w-md text-gray-300">
+
+        <p className="fade sm:text-lg sm:text-center text-sm text-right mb-8 max-w-md text-gray-300">
           نحن متخصصون في تحويل المواقع الإلكترونية والبرامج إلى منصات حديثة تركز
           على المستخدم وتزيد من التفاعل والنمو.
         </p>
